9de92f473a0eff8ce6fca80c3227cf93e4760f25
Cookie Queen <linh.nguyenquoc@gmx.de>
2024-02-24 11:22:51 +0100

    meine skills

--- resources/js/personalPortfolio.js
+++ resources/js/personalPortfolio.js
@@ -103,3 +103,31 @@ document.addEventListener('DOMContentLoaded', function() {
         codingIcon.style.height = '3rem';
     });
 });
+
+// scrollen von skills
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector(".all-skills");
+    const buttonNext = document.querySelector(".swiper-button.vorwärts");
+    const buttonBack = document.querySelector(".swiper-button.rückwärts");
+    const containerWidth = container.offsetWidth;
+    // Breite .skill-container
+    const skillWidth = document.querySelector(".skill-container").offsetWidth;
+    // Handler für Klick auf den Vorwärtspfeil
+    buttonNext.addEventListener("click", function() {
+        // Anzahl der sichtbaren .skill-container-Elemente
+        const visibleSkills = Math.floor(containerWidth / skillWidth);
+        // Anzahl der verschobenen .skill-container-Elemente
+        const scrollAmount = skillWidth * visibleSkills;
+        // Scrolle den Container um die Breite eines .skill-container-Elements nach rechts
+        container.scrollLeft += scrollAmount;
+    });
+    // Handler für Klick auf den Rückwärtspfeil
+    buttonBack.addEventListener("click", function() {
+        // Anzahl der sichtbaren .skill-container-Elemente
+        const visibleSkills = Math.floor(containerWidth / skillWidth);
+        // Anzahl der verschobenen .skill-container-Elemente
+        const scrollAmount = skillWidth * visibleSkills;
+        // Scrolle den Container um die Breite eines .skill-container-Elements nach links
+        container.scrollLeft -= scrollAmount;
+    });
+});
